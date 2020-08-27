c3003769b4483c5053d3d1d993cfcebde49c6cd9
Voggastur <v3x0r_x014@hotmail.com>
2020-08-27 22:31:02 +0000

    fixed products

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HKrsTDj8tSUqbzv9FPGpnTiPfy1ijN5DeClKA9EET4Lw8KcyumUSyCSqTK9K7iXy0ogQmwVFsALWvY5SPgquRyV004hmAhJgL',
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
     }
 
     return render(request, template, context)
