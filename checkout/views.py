e175795fdb80392032f705e478a337b82a2361a5
Voggastur <v3x0r_x014@hotmail.com>
2020-08-27 21:27:20 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HKrsTDj8tSUqbzv9FPGpnTiPfy1ijN5DeClKA9EET4Lw8KcyumUSyCSqTK9K7iXy0ogQmwVFsALWvY5SPgquRyV004hmAhJgL',
     }
 
     return render(request, template, context)
