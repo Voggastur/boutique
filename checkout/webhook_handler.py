d9dff413751cbc80dd5a4b3a0cf90e5d95a3072f
Voggastur <v3x0r_x014@hotmail.com>
2020-08-28 14:02:40 +0000

    added basic webhook functionality

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def payment_intent_succeeded(self, event):
+        """
+        Handle athe payment_intent.succeeded webhook from stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+
