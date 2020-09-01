ee8f62f2607b907a89133619a2a452efeb893966
Voggastur <v3x0r_x014@hotmail.com>
2020-09-01 16:51:52 +0000

    added profile app

--- boutique/settings.py
+++ boutique/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado.urls'
+ROOT_URLCONF = 'boutique.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -113,7 +113,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'boutique.wsgi.application'
 
 
 # Database
